0e0c994763fd0279023d9186e64d824e77f95e7c
Martin <joewwi03@gmail.com>
2023-03-18 12:43:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const blockLength = 264; 
+const freeSampleDistance = 400; 
+
+function distanceFromHqInBlocks(distanceInBlocks) {
+  return Math.abs(distanceInBlocks - 42);
+}
+
+function distanceFromHqInFeet(distanceInBlocks) {
+  return distanceFromHqInBlocks(distanceInBlocks) * blockLength;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const distanceInBlocks = Math.abs(endBlock - startBlock);
+  return distanceInBlocks * blockLength;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  const distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+  let farePrice;
+
+  if (distanceInFeet <= freeSampleDistance) {
+    farePrice = 0;
+  } else if (distanceInFeet > freeSampleDistance && distanceInFeet <= 2000) {
+    farePrice = (distanceInFeet - freeSampleDistance) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    farePrice = 25;
+  } else {
+    farePrice = "cannot travel that far";
+  }
+
+  return farePrice;
+}
